52de1705b53d1d3cbcc94928029fbb4686fc094e
bhimireddysivasaranya <saranyabhimireddy@gmail.com>
2025-05-26 10:04:22 +0530

    Add files via upload

---
+++ Maven_Minded/Maven-master/src/main/java/com/example/aiapp/TextRequest.java
@@ -0,0 +1,14 @@
+
+package com.example.aiapp;
+
+public class TextRequest {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
